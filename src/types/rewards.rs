3073bd10737c2b7c5eb13e2ea82c78e0392785e1
jaymalave <jaym.github@gmail.com>
2025-08-27 19:03:18 -0600

    feat: add missing rewards types

--- src/types/rewards.rs
+++ src/types/rewards.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{contracttype, U256};
+use soroban_sdk::{contracttype, Address, U256};
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -12,4 +12,37 @@ pub struct GlobalRewardsConfig {
 pub struct PoolRewardInfo {
     pub processed: bool,
     pub total_liquidity: U256,
+}
+
+// Incentives configuration for a specific pool.
+#[derive(Clone)]
+#[contracttype]
+pub struct PoolIncentiveConfig {
+    pub reward_tps: u128,
+    pub reward_expired_at: u64,
+}
+
+// Mutable pool incentive data that evolves over time.
+#[derive(Clone)]
+#[contracttype]
+pub struct PoolIncentiveData {
+    // rewards
+    pub block: u64,
+    pub accumulated_rewards: u128,
+    pub claimed_rewards: u128,
+    pub rewards_last_time: u64,
+    // lp fees - Tracks how much of token_b has been collected as fees per unit of LP token, cumulatively.
+    pub fee_growth_per_lp: u128,
+}
+
+// Per-user incentive data.
+#[derive(Clone)]
+#[contracttype]
+pub struct UserIncentiveData {
+    // rewards
+    pub pool_accumulated_rewards: u128,
+    pub rewards_to_claim: u128,
+    pub last_block: u64,
+    // lp fees
+    pub fee_checkpoint: u128,
 }
